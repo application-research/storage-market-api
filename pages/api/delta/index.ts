ac1823a021d8c06d9f703f51b62056bcd97ddb1d
silversword <silversword@SILVERSWORD.local>
2023-05-04 17:44:06 -0700

    pull latest stats from scrape

--- pages/api/delta/index.ts
+++ pages/api/delta/index.ts
@@ -5,10 +5,7 @@ import DB from '@common/db';
 export default async function APIGlobalDeltaIndex(req, res) {
   await Server.cors(req, res);
 
-  const response = await DB.select()
-    .from('delta_analytics')
-    .orderBy('created_at', 'asc')
-    .first();
+  const response = await DB.select().from('delta_analytics').orderBy('created_at', 'desc').first();
 
   res.json({ global: response });
 }
